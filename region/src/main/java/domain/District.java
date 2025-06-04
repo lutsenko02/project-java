f2454ac3278aae5dd16bd997814ee7d0e4ddbfc5
lutsenko02 <75694397+lutsenko02@users.noreply.github.com>
2025-06-04 12:38:37 +0300

    dev3

---
+++ region/src/main/java/domain/District.java
@@ -0,0 +1,80 @@
+package domain;
+/**
+* Класс данных о районах
+*/
+public class District {
+// Идентификатор района
+private Long id;
+// Наименование района
+private String namedistrict;
+//Площадь района
+private String districtArea;
+//Год застройки
+private String districYear;
+//Кличество населения
+private int districNumberOfPeople;
+
+public District() {
+}
+
+public District(String namedistrict) {
+	this.namedistrict = namedistrict;
+}
+
+public District(Long id, String namedistrict, String districtArea, String districYear, int districNumberOfPeople) {
+	this.id = id;
+	this.namedistrict = namedistrict;
+	this.districtArea = districtArea;
+	this.districYear = districYear;
+	this.districNumberOfPeople = districNumberOfPeople;
+}
+
+public Long getId() {
+return id;
+}
+
+public void setId(Long id) {
+this.id = id;
+}
+
+//Геттеры и сеттеры
+
+public String getNameDistrict() {
+	return namedistrict;
+}
+public void setnamedistrict(String namedistrict) {
+	this.namedistrict = namedistrict;
+}
+
+public String getDistrictArea() {
+	return districtArea;
+}
+public void setDistrictArea(String districtArea) {
+	this.districtArea = districtArea;
+}
+
+public String getDistrictYear() {
+	return districYear;
+}
+public void setDistrictYear(String districYear) {
+	this.districYear = districYear;
+}
+
+public int getDistrictNumberOfPeople() {
+	return districNumberOfPeople;
+}
+public void setDistrictNumberOfPeople(int districNumberOfPeople) {
+	this.districNumberOfPeople = districNumberOfPeople;
+}
+
+
+@Override
+public String toString() {
+return "District {" +
+		"Id = " + id + 
+		", namedistrict = " + namedistrict +
+		", districtArea = " + districtArea +
+		", districYear = " + districYear +
+		", districNumberOfPeople = " + districNumberOfPeople + "}";
+}
+}
